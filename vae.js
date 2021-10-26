bd0285c46954a66f34a3ee2c8518ed18c4c76da3
AlisonW97 <69679720+AlisonW97@users.noreply.github.com>
2021-10-26 14:59:47 +0800

    Update vae.js

--- vae.js
+++ vae.js
@@ -7,7 +7,7 @@
 const url = `https://api1-xusong.taihe.com/auth/`;
 const method = `POST`;
 const headers = {
-'Cookie' : `JSESSID=7f5efe81c3bea13d58DF19c554ac9c8611b8c789c0`,
+'Cookie' : `JSESSID=dcd92079e874a3ff58DF1f85da00478361a512fd8f`,
 'Accept' : `*/*`,
 'Connection' : `keep-alive`,
 'Content-Type' : `application/x-www-form-urlencoded`,
@@ -16,7 +16,7 @@ const headers = {
 'User-Agent' : `VaeHome/2.5.2 (iPhone; iOS 13.5; Scale/2.00)`,
 'Accept-Language' : `zh-Hans-FR;q=1`
 };
-const body = `q=4/2ZMy33NIhR1PUCFG0XZyoXJY5eYAE/5e0cHCnyoFiEzw0QSznZSqbs1lynK48W37HDs27xixCfLrVg4pkIsqpYc1TzFp7bDlNsvir70GR3lcBwz4WTpMmbS8E2naCK9At2ypNqt5lVV3T5MBjlkkCrom2vi838EH/k/L7IxKUl/CCjF8HJhLFhVTDZEPmOr2Y%2BCtH0tYod61syw5G94dX6Ukiz7r04KZB4UeKH0zcSpDof%2BUimAzfIJ0Ozs52s0D8Moil9fy4w7AhFAUqhb/jZ1q%2BJy9Y77%2BRN4gUjMbw6%2BALDKSPOU3Ad/qeOCWucriq1k8Rk54yhR6NLw9T9caf1qV48S2b44mvuv%2Bc7F%2BHdB0ZumFgQwHiJ6VklImhIKVAOI25IyDti0jWPMS/cv47fZ8A0qPvzTn1pK70rU8shWmi%2B7PPJ0xnJ9kbQSQBw1Ib124w1Qudb9voqBUb3136FJaWgHdjNRIe3jJ5%2BTD4hojreCpfdYgrCYr1GcGqawcTspYprb%2B14sjDZAbXJUkwmM4F7T%2BHze68Y4i%2B563rWWDgDa2s8n4ugUh2Tsnb8g4eg4HYapKQo0E0dNb/PHBpWmf685obzoJXjrxu02cpS3xj/9/eOckC0LSDVU8/i%2BCPw0RAYAbe/jF3onZlVlDWv0hreWi64TckeoEeaL7mGuDdkDMstwbWmXjjGrNe%2BoV8ziEw%2BabXHrB2kE8T%2BQ65lWumSBm2EqApwiUGVSHfMU%2Brc2hMylG9XuK23Q%2B03TXayrGvDEfvl3l0r44KGEKyX%2BjM%2Bdk3do/fI0zvKXnKvL8aQ9im2vA1afAartPzJjUl3VddoX23Ou3NbEBHkOw%3D%3D`;
+const body = `q=xIaqqcUE6zbRJaRQzLIbR08W3IBm4EixkSrkIAcB/x0PJ0ueTkPCqA%2BPXfXMsiyXlR9sCKMcZ1JYpIe9hBobSevfaxzZxYOJc1DBoWY3hC2hx8DXPVWbCVSeY/4636B%2BhAKey0wGrrO4NnvI8ZpnCualnGYG4wIobMdZf7M2GBQDz%2Bt1F2hzSc0y4aYQz1Isk1LXyIcxQmEqhYYRQd2mJkLsF9ishT7RoXH4eI4pcaJL44ZMtwj8iSneIl6O%2BW69IoriCwqW94%2BTNJ0FzbPnw12v6zEy3Erkv9pm4gpT6Cu%2ByJFOHmsRZ3B0n/CAh1k1cdYf0UHqpZ6HSYSXmflGAVmWQs8PQxCyF%2BvC%2BoeEh56P3einrPk0TAG%2BWlKNXVt/cOnB5T8gY91vEGPTYDW90RaoxhyuNyt2qkp%2BgIu/AIOqhrJ%2Bl/aUjnP9eghlLwHwtrooMT%2BE/BE0DvLKN75%2BQNSX6DScSSlgKGpilT3M4qS8Xcs09afay4zQjtgJxBluTGjnO3UZXfjrS/BNdqjFP5F9ulunsfDRjJ4013R3OLa3wZZ9ZKwRhjFuAJDyj0IXF3KdIHcFNtyRxtYkKI1Rc9QZlvlcue9AEcugZez2IONMInniEmikhi66kgUSATsB6mIEG1rnv5I/eybxju5MdColj3xCv/gkjsF3C6gWr0O1IfywIiqcVAn%2Bxi/HS/ema1b/6yj20gf3NHa2mX%2BPAwpSGa37vCQSqhwv4DDW9ILCmE4eACgDADjLn1hkgqSsrBiIzn/lao3xNKGqqVF4K%2BIEfhdy6KpbO%2BJ6l%2BbDW0FGyrgu1CDsPdaKXzDk5oUNtOT2kNVja9Nd2PtbUjyB3w%3D%3D`;
 
 const myRequest = {
     url: url,
